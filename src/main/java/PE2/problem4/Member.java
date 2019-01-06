7a47117b8689129f986889b72f29d3472d134b2b
meghanadiwakar <meghana567naidu@gmail.com>
2019-01-06 09:59:16 +0530

    Added java code

--- src/main/java/PE2/problem4/Member.java
+++ src/main/java/PE2/problem4/Member.java
@@ -1,8 +1,36 @@
 package PE2.problem4;
 
-public class Member {
-        String Name;
-        int Age;
-        double Salary;
+
+public class Member
+{
+    private String name;
+    private int age;
+    private String salary;
+
+    public String getName() {
+        return name;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSalary() {
+        return salary;
+    }
+
+    public void setSalary(String salary) {
+        this.salary = salary;
+    }
+
+
+}
 
